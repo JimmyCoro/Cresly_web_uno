a67bd62ef1c5763bf9a21df48fcae604657bbf07
Jimmy Coro <corojimmy@gmail.com>
2024-08-24 20:31:20 -0500

    Deccc

--- cresly_pizzeria/usuarios/forms.py
+++ cresly_pizzeria/usuarios/forms.py
@@ -2,8 +2,38 @@ from django import forms
 from .models import Cliente
 
 class ClienteForm(forms.ModelForm):
-    password = forms.CharField(widget=forms.PasswordInput)
+    password = forms.CharField(
+        widget=forms.PasswordInput(attrs={
+            'class': 'form-control',
+            'placeholder': 'Contraseña',
+            'data-icon': 'bi-lock'  # Añadir un atributo personalizado para el icono
+        })
+    )
+    
+    correo = forms.EmailField(
+        widget=forms.EmailInput(attrs={
+            'class': 'form-control',
+            'placeholder': 'Correo electrónico',
+            'data-icon': 'bi-envelope'  # Icono para correo electrónico
+        })
+    )
+    
+    nombre = forms.CharField(
+        widget=forms.TextInput(attrs={
+            'class': 'form-control',
+            'placeholder': 'Nombre',
+            'data-icon': 'bi-person'  # Icono para nombre
+        })
+    )
+    
+    apellido = forms.CharField(
+        widget=forms.TextInput(attrs={
+            'class': 'form-control',
+            'placeholder': 'Apellido',
+            'data-icon': 'bi-person'  # Icono para apellido
+        })
+    )
+
     class Meta:
         model = Cliente
-        fields = ['correo', 'nombre', 'apellido']
-    
+        fields = ['correo', 'nombre', 'apellido', 'password']
